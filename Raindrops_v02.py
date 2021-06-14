9c227328dbe53f9cd145e04ecb7b734b2e8f11d6
djweissenborn <workswithmodels@gmail.com>
2021-06-14 15:15:40 -0500

    Changed constants to SNAKE_UPPER_CASE to conform to PEP8 style guide and trial Git functionality.

--- Raindrops_v02.py
+++ Raindrops_v02.py
@@ -11,13 +11,15 @@ import random
 
 pygame.init()
 
-frame_width  = 600
-frame_height = 800
+FRAME_WIDTH  = 600
+FRAME_HEIGHT = 800
 
-black = (0, 0, 0)
-white = (255, 255, 255)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
 
-gameDisplay = pygame.display.set_mode((frame_width, frame_height))
+FPS = 60
+
+gameDisplay = pygame.display.set_mode((FRAME_WIDTH, FRAME_HEIGHT))
 pygame.display.set_caption('Raindrops')
 clock = pygame.time.Clock()
 
@@ -39,14 +41,14 @@ class Droplet:
         self.has_dropped = has_dropped
 
 def rain(Droplet): #don't think this works yet, but I would rather pass the function a Droplet than its parts.
-    pygame.draw.circle(gameDisplay, black, (Droplet.x_pos, Droplet.y_pos), Droplet.radius)
+    pygame.draw.circle(gameDisplay, BLACK, (Droplet.x_pos, Droplet.y_pos), Droplet.radius)
 
 def game_loop():
 
     # objs = []
-    # objs = [Droplet(random.randrange(0, frame_width), random.randrange(0, frame_height), 3, False) for i in range(10)]
+    # objs = [Droplet(random.randrange(0, FRAME_WIDTH), random.randrange(0, FRAME_HEIGHT), 3, False) for i in range(10)]
 
-    # gameDisplay.fill(white)
+    # gameDisplay.fill(WHITE)
     # for i in range(10):
     #     rain(objs[i].x_pos, objs[i].y_pos, objs[i].radius)
     
@@ -64,13 +66,13 @@ def game_loop():
                 #quit()
 
         
-        objs.append(Droplet(random.randrange(0, frame_width), random.randrange(0, frame_height), 5, False))
-        gameDisplay.fill(white)
+        objs.append(Droplet(random.randrange(0, FRAME_WIDTH), random.randrange(0, FRAME_HEIGHT), 5, False))
+        gameDisplay.fill(WHITE)
         for i in range(len(objs)):
             #rain(objs[i].x_pos, objs[i].y_pos, objs[i].radius)
             rain(objs[i])
         pygame.display.update()
-        clock.tick(60)
+        clock.tick(FPS)
 
 game_loop()
 pygame.quit()
